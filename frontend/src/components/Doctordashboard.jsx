c7862a19b2d40b64935d585046bc8f2bece8c9eb
Irshad3786 <167470522+Irshad3786@users.noreply.github.com>
2024-11-01 01:16:35 +0530

    Update Doctordashboard.jsx

--- frontend/src/components/Doctordashboard.jsx
+++ frontend/src/components/Doctordashboard.jsx
@@ -34,7 +34,7 @@ function Doctordashboard() {
       if(res.data.status){
         
       }else{
-        navigate("/Signup)
+        navigate("/Signup")
       }
     })
     .catch(error=>console.log(error))
