d2faad3e954e7e3644de4a593a41aff9efa03f21
IRSHAD <167470522+Irshad3786@users.noreply.github.com>
2024-11-07 19:33:32 +0000

    size change

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -14,7 +14,7 @@ function Signup() {
     margin: "0 auto",
     borderColor: "red",
     position: "absolute",
-    top: "50%",
+    top: "80%",
     left: "50%",
     transform: "translate(-50%, -50%)",
   };
@@ -92,7 +92,7 @@ function Signup() {
             color={"magenta"}
             loading={loading}
             cssOverride={override}
-            size={25}
+            size={30}
             aria-label="Loading Spinner"
             data-testid="loader"
           />
